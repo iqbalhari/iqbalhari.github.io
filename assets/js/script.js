7a16979ffc78d0054a08e3156d9ac0164f7fe9bc
iqbalhari <iqbalhariarrasyid2005@gmail.com>
2024-04-06 05:43:56 +0700

    background image body update

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ menuLink.addEventListener("click", function(e) {
 })
 
 let images = [
-    'bg1.jpg','bg2.jpeg','bg3.jpg','bg4.jpg','bg5.jpg','bg6.jpg','bg7.jpg','bg8.jpg','bg9.jpg','bg10.jpg','bg11.jpg','bg12.jpg','bg13.jpg','bg14.jpg','bg15.jpg','bg16.jpg','bg17.jpg','bg18.jpg','bg19.jpg'
+    'bg1.jpg','bg2.jpeg','bg4.jpg','bg5.jpg','bg9.jpg','bg12.jpg'
 ]; // Ganti nama gambar sesuai dengan yang Anda miliki
 
 let currentIndex = 0;
@@ -37,7 +37,7 @@ let currentIndex = 0;
 function changeImage() {
   document.body.style.backgroundImage = `url('/assets/img/bg/${images[currentIndex]}')`;
   currentIndex = (currentIndex + 1) % images.length;
-  document.body.style.transition = "2s";
+  document.body.style.transition = "1.5s";
 }
 
 changeImage(); // Pertama kali, panggil fungsi changeImage agar gambar berubah segera
